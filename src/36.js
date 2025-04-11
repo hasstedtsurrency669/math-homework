927c73b6a7353fa2c438dc7198ca21d80ea79573
hasstedtsurrency669 <hasstedtsurrency669@gmail.com>
2025-04-11 12:37:02 +0000

    Add 36.js

---
+++ src/36.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+let randomNumber = getRandomInt(10) + getRandomInt(5);
+console.log(randomNumber); // Output: 12
