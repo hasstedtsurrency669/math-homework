a3d4232d6eec1732c76d07ae90d66782356936c5
hasstedtsurrency669 <hasstedtsurrency669@gmail.com>
2025-04-11 12:47:03 +0000

    Add 41.js

---
+++ src/41.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math Homework</title>
+</head>
+<body>
+    <h1>Solving Math Problems with Python</h1>
+    <p>Here's a simple JavaScript code to help you solve math problems using Python:</p>
+
+    <script type="text/javascript">
+        // Example math problem
+        var num1 = 5;
+        var num2 = 3;
+        var operation = "+";
+        var result;
+
+        if (operation === "+") {
+            result = num1 + num2;
+        } else if (operation === "-") {
+            result = num1 - num2;
+        } else if (operation === "*") {
+            result = num1 * num2;
+        } else if (operation === "/") {
+            result = num1 / num2;
+        }
+
+        document.body.innerHTML += `<p>The solution is: ${result}</p>`;
+    </script>
+</body>
+</html>
